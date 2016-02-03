fa6486bd3fb53cad968e3b63370b18ccd45262ce
mlantz <mattlantz@gmail.com>
2016-02-03 08:50:16 -0500

    Switching to gift cards over coupons

--- src/QuarxViews/menu.blade.php
+++ src/QuarxViews/menu.blade.php
@@ -2,4 +2,4 @@
 <li><a href="<?= URL::to('quarx/transactions'); ?>"><span class="fa fa-dollar"></span> Transactions</a></li>
 <li><a href="<?= URL::to('quarx/subscriptions'); ?>"><span class="fa fa-credit-card"></span> Subscriptions</a></li>
 <li><a href="<?= URL::to('quarx/orders'); ?>"><span class="fa fa-ship"></span> Orders</a></li>
-<li><a href="<?= URL::to('quarx/coupons'); ?>"><span class="fa fa-ticket"></span> Coupons</a></li>
+<li><a href="<?= URL::to('quarx/gift-cards'); ?>"><span class="fa fa-ticket"></span> Gift Cards</a></li>
