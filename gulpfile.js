139db38bfc7b366741bf0292d985bbbbbba966c6
mlantz <mattlantz@gmail.com>
2016-12-31 02:22:14 -0500

    gulp cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -4,39 +4,6 @@ elixir.config.publicPath = 'src/Assets/dist/';
 elixir.config.assetsPath = 'src/Assets/';
 
 elixir(function(mix) {
-    mix.styles([
-        '../packages/dropzone/basic.css',
-        '../packages/dropzone/dropzone.css',
-        '../packages/redactor/redactor.css',
-        '../packages/datepicker/bootstrap-datetimepicker.css',
-        'raw.min.css',
-        'device.css',
-        'main.css',
-        'loaders.css',
-        'modules.css',
-    ]);
-
-    mix.scripts([
-        'bootstrap.min.js',
-        'quarx.js',
-        'forms.js',
-        'typeahead.bundle.js',
-        'bootstrap-tagsinput.min.js',
-        '../packages/dropzone/dropzone.js',
-        '../packages/datepicker/moment.js',
-        '../packages/datepicker/bootstrap-datetimepicker.min.js',
-        '../packages/redactor/redactor.js',
-        '../packages/redactor/filemanager.js',
-        '../packages/redactor/fontcolor.js',
-        '../packages/redactor/fontfamily.js',
-        '../packages/redactor/fontsize.js',
-        '../packages/redactor/imagemanager.js',
-        '../packages/redactor/stockimagemanager.js',
-        '../packages/redactor/specialchar.js',
-        '../packages/redactor/table.js',
-        '../packages/redactor/video.js',
-
-        'modules.js',
-        'dropzone-custom.js',
-    ]);
+    mix.styles([]);
+    mix.scripts([]);
 });
